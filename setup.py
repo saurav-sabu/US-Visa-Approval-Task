45794d524259a49e101bccc116651da2a6fc6622
Saurav Sabu <saurav.sabu9@gmail.com>
2024-02-21 21:58:13 +0530

    setup.py code updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= "US-Visa-Approval-Task",
+    version= "0.0.0",
+    author= "Saurav Sabu",
+    author_email= "saurav.sabu9@gmail.com",
+    packages= find_packages()  
+)
