e43663e57d742ebd872748f10b8afe891f2426a4
Saurav Sabu <saurav.sabu9@gmail.com>
2024-03-04 16:56:18 +0530

    corrected read_yaml_file method

--- US_Visa_Approval/utils/main_utils.py
+++ US_Visa_Approval/utils/main_utils.py
@@ -8,7 +8,7 @@ import os
 def read_yaml_file(file_path:str):
     try:
         with open(file_path,"rb") as file:
-            return yaml.safe.load(file)
+            return yaml.safe_load(file)
     
     except Exception as e:
         raise USVisaException(e,sys)
