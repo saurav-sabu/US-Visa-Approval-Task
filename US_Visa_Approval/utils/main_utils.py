0bc827627d89e9e774feeba1ace52d6d1bc27a6e
Saurav Sabu <saurav.sabu9@gmail.com>
2024-03-04 11:01:00 +0530

    In main_utils.py write_yaml_file method code updated

--- US_Visa_Approval/utils/main_utils.py
+++ US_Visa_Approval/utils/main_utils.py
@@ -3,11 +3,26 @@ from US_Visa_Approval.logger import logging
 
 import sys
 import yaml
+import os
 
 def read_yaml_file(file_path:str):
     try:
         with open(file_path,"rb") as file:
             return yaml.safe.load(file)
+    
+    except Exception as e:
+        raise USVisaException(e,sys)
+    
+
+def write_yaml_file(file_path:str,content:object,replace:bool=False):
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path),exist_ok=True)
+
+        with open(file_path,"w") as file:
+            yaml.dump(content,file)
     
     except Exception as e:
         raise USVisaException(e,sys)
